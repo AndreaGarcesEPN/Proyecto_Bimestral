65b24b10d0503a0a462ffb9520a420d2f64bfae7
Kevin Vinlasaca <50632360+Kevin-Vinlasaca@users.noreply.github.com>
2019-06-24 11:21:39 -0500

    menu

--- loginprueba/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ loginprueba/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -82,12 +82,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(483, 189);
+            this.BackColor = System.Drawing.Color.MediumOrchid;
+            this.ClientSize = new System.Drawing.Size(504, 247);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.AppStarting;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
