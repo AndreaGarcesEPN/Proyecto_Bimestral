7b0e230a95176bcf3b5dd724a0ec3bceba988058
Kevin Vinlasaca <50632360+Kevin-Vinlasaca@users.noreply.github.com>
2019-07-19 22:54:18 -0500

    imagen  en login
    en el login se evita maximizar

--- Login/Sln Proyecto Login/Proyecto Login/Form1.Designer.cs
+++ Login/Sln Proyecto Login/Proyecto Login/Form1.Designer.cs
@@ -44,27 +44,30 @@
             // checkMostrar
             // 
             this.checkMostrar.AutoSize = true;
-            this.checkMostrar.Location = new System.Drawing.Point(179, 131);
+            this.checkMostrar.BackColor = System.Drawing.Color.Transparent;
+            this.checkMostrar.ForeColor = System.Drawing.SystemColors.Menu;
+            this.checkMostrar.Location = new System.Drawing.Point(135, 109);
             this.checkMostrar.Name = "checkMostrar";
             this.checkMostrar.Size = new System.Drawing.Size(91, 17);
             this.checkMostrar.TabIndex = 11;
             this.checkMostrar.Text = "Mostrar Clave";
-            this.checkMostrar.UseVisualStyleBackColor = true;
+            this.checkMostrar.UseVisualStyleBackColor = false;
             this.checkMostrar.CheckedChanged += new System.EventHandler(this.CheckMostrar_CheckedChanged);
             // 
             // BtnIngresar
             // 
-            this.BtnIngresar.Location = new System.Drawing.Point(195, 170);
+            this.BtnIngresar.BackColor = System.Drawing.Color.Transparent;
+            this.BtnIngresar.Location = new System.Drawing.Point(151, 148);
             this.BtnIngresar.Name = "BtnIngresar";
             this.BtnIngresar.Size = new System.Drawing.Size(75, 23);
             this.BtnIngresar.TabIndex = 10;
             this.BtnIngresar.Text = "Ingresar";
-            this.BtnIngresar.UseVisualStyleBackColor = true;
+            this.BtnIngresar.UseVisualStyleBackColor = false;
             this.BtnIngresar.Click += new System.EventHandler(this.BtnIngresar_Click);
             // 
             // textBoxClave
             // 
-            this.textBoxClave.Location = new System.Drawing.Point(179, 105);
+            this.textBoxClave.Location = new System.Drawing.Point(135, 83);
             this.textBoxClave.Name = "textBoxClave";
             this.textBoxClave.Size = new System.Drawing.Size(164, 20);
             this.textBoxClave.TabIndex = 9;
@@ -72,7 +75,7 @@
             // 
             // textBoxUsuario
             // 
-            this.textBoxUsuario.Location = new System.Drawing.Point(179, 58);
+            this.textBoxUsuario.Location = new System.Drawing.Point(135, 36);
             this.textBoxUsuario.Name = "textBoxUsuario";
             this.textBoxUsuario.Size = new System.Drawing.Size(164, 20);
             this.textBoxUsuario.TabIndex = 8;
@@ -80,7 +83,9 @@
             // Usuario
             // 
             this.Usuario.AutoSize = true;
-            this.Usuario.Location = new System.Drawing.Point(80, 61);
+            this.Usuario.BackColor = System.Drawing.Color.Transparent;
+            this.Usuario.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Usuario.Location = new System.Drawing.Point(36, 39);
             this.Usuario.Name = "Usuario";
             this.Usuario.Size = new System.Drawing.Size(43, 13);
             this.Usuario.TabIndex = 6;
@@ -89,7 +94,9 @@
             // Contraseña
             // 
             this.Contraseña.AutoSize = true;
-            this.Contraseña.Location = new System.Drawing.Point(80, 108);
+            this.Contraseña.BackColor = System.Drawing.Color.Transparent;
+            this.Contraseña.ForeColor = System.Drawing.SystemColors.Control;
+            this.Contraseña.Location = new System.Drawing.Point(36, 86);
             this.Contraseña.Name = "Contraseña";
             this.Contraseña.Size = new System.Drawing.Size(61, 13);
             this.Contraseña.TabIndex = 7;
@@ -97,9 +104,10 @@
             // 
             // imgUsuario
             // 
+            this.imgUsuario.BackColor = System.Drawing.Color.Transparent;
             this.imgUsuario.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("imgUsuario.BackgroundImage")));
             this.imgUsuario.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.imgUsuario.Location = new System.Drawing.Point(145, 58);
+            this.imgUsuario.Location = new System.Drawing.Point(101, 36);
             this.imgUsuario.Name = "imgUsuario";
             this.imgUsuario.Size = new System.Drawing.Size(28, 20);
             this.imgUsuario.TabIndex = 12;
@@ -107,9 +115,10 @@
             // 
             // ImgContraseña
             // 
+            this.ImgContraseña.BackColor = System.Drawing.Color.Transparent;
             this.ImgContraseña.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ImgContraseña.BackgroundImage")));
             this.ImgContraseña.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ImgContraseña.Location = new System.Drawing.Point(147, 108);
+            this.ImgContraseña.Location = new System.Drawing.Point(103, 86);
             this.ImgContraseña.Name = "ImgContraseña";
             this.ImgContraseña.Size = new System.Drawing.Size(26, 17);
             this.ImgContraseña.TabIndex = 13;
@@ -119,7 +128,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(493, 321);
+            this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(359, 227);
             this.Controls.Add(this.ImgContraseña);
             this.Controls.Add(this.imgUsuario);
             this.Controls.Add(this.checkMostrar);
@@ -128,7 +140,12 @@
             this.Controls.Add(this.textBoxUsuario);
             this.Controls.Add(this.Contraseña);
             this.Controls.Add(this.Usuario);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
             ((System.ComponentModel.ISupportInitialize)(this.imgUsuario)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.ImgContraseña)).EndInit();
