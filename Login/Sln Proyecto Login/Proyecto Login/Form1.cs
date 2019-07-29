233393d2a7cf1e49bbc6e83d9976ae90d7a17bdb
Kevin Vinlasaca <50632360+Kevin-Vinlasaca@users.noreply.github.com>
2019-07-29 00:18:17 -0500

    carpeta user y admin

--- Login/Sln Proyecto Login/Proyecto Login/Form1.cs
+++ Login/Sln Proyecto Login/Proyecto Login/Form1.cs
@@ -26,29 +26,52 @@ namespace Proyecto_Login
         {
             string usuario = textBoxUsuario.Text;
             string clave = textBoxClave.Text;
-            string clave_valida;
-            string usuarios = @"D:\Proyectos progra\Proyecto_Bimestral\Proyecto_Bimestral\Login\admin\" + usuario + @".txt";
-            if (File.Exists(usuarios))
+            string contraseñaadmin;
+            string contraseñauser;
+
+            string admin = @"D:\Proyectos progra\Proyecto_Bimestral\Proyecto_Bimestral\Login\admin\" + usuario + @".txt";
+            string usuarios = @"D:\Proyectos progra\Proyecto_Bimestral\Proyecto_Bimestral\Login\usuarios\" + usuario + @".txt";
+            if (File.Exists(admin) )
             {
-                clave_valida = File.ReadAllText(usuarios);
-                if (clave.Equals(clave_valida))
+                //si exixte el archivo lo abre y lo lee lo compara con la contraseña y lo acepta
+               contraseñaadmin = File.ReadAllText(admin);
+                
+                if (clave.Equals(contraseñaadmin) )
                 {
                     MenuPrincipal menu = new MenuPrincipal();
                     menu.Visible = true;
                     Visible = false;
-
-
-
                 }
                 else
                 {
                     MessageBox.Show("contraseña erronea");
                 }
+                
 
             }
             else
             {
-                MessageBox.Show("usuario no registrado");
+                if (File.Exists(usuarios))
+                {
+                    //si exixte el archivo lo abre y lo lee lo compara con la contraseña y lo acepta
+                    contraseñauser = File.ReadAllText(usuarios);
+
+                    if (clave.Equals(contraseñauser))
+                    {
+                        MenuPrincipal menu = new MenuPrincipal();
+                        menu.Visible = true;
+                        Visible = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("contraseña erronea");
+                    }
+                }else {
+                    MessageBox.Show(" no existe el usuario ");
+
+                }
+                
+            
             }
 
         
